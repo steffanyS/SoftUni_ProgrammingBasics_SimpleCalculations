2d76a49e07fb09635733a789b975dea7260425ee
steffanyS <stefani.sabcheva@gmail.com>
2017-01-17 18:56:47 +0200

    added exercises

---
+++ Exersices/InchesCentimeters/InchesCentimeters/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InchesCentimeters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("inches = ");
+            var inches = double.Parse(Console.ReadLine());
+            var cm = inches * 2.54;
+            Console.WriteLine("Centimeters = " + cm);
+        }
+    }
+}
